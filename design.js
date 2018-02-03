76b60637b4f720a6c05f9ac0f34c608db3cc20a9
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-02-03 17:19:06 +0100

    added validation of login

--- design.js
+++ design.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
-$("#error").hide();
-  $("#button").click(function() {
-    $("#error").show();
-  });
-});
+$('#button').click(function() {
+       var username=$("#usr").val();
+       var password=$("#psw").val();
+
+       if(username.length<=1 || password.length<=1){
+       	$("#error").show();
+
+
+       	}
+       	else { 
+       		$("#error").hide();
+       	}
+       	 
+
+       
+});//click
+});// ready
 
   
 
