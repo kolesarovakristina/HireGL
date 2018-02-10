2fd0d35f5de5395e9f9c708cfd1b7e7bb8a2e1cd
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-02-10 17:29:39 +0100

    Added validation and ajax

--- design.js
+++ design.js
@@ -1,21 +1,56 @@
-$(document).ready(function() {
-$('#button').click(function() {
-       var username=$("#usr").val();
-       var password=$("#psw").val();
+$(document).ready(function(){ 
 
-       if(username.length<=1 || password.length<=1){
-       	$("#error").show();
+  $("#button").click(function(){
+  	    var username=$("#usr").val();
+  	    var password=$("#psw").val();
+    if(username.trim().length<2 || password.trim().length<2 ){
+    $("#error").show();
+    
+    }
+
+    else
+    {
+$("#error").hide();
 
 
-       	}
-       	else { 
-       		$("#error").hide();
-       	}
-       	 
 
-       
-});//click
-});// ready
 
-  
 
+
+    }
+    });
+  $("#button").click(function(){
+    var userNameValue=$("#usr").val();
+    var passwordValue=$("#psw").val();
+  $.ajax({
+    
+    url:"http://akademiasovy.ddns.net:8081/api/auth/login",
+    type: 'POST',
+    contentType:'application/json',
+    data: JSON.stringify({"userName":userNameValue,"password":passwordValue}),
+    success: function(data){
+        data.token
+        window.location.href='editInterview.html';
+    },
+    error: function(){
+        
+$("#error").show();
+    
+
+    }
+
+
+
+
+
+
+
+  });
+});
+
+
+
+      
+
+
+});
